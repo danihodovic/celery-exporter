3e27346826e5888242d193b63f5c41382a98f7be
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2021-03-22 22:54:04 +0000

    Add return type.

--- src/helpers.py
+++ src/helpers.py
@@ -3,6 +3,6 @@ import re
 exception_pattern = re.compile(r"^(\w+)\(")
 
 
-def get_exception_class(exception_name: str):
+def get_exception_class(exception_name: str) -> str:
     m = exception_pattern.match(exception_name)
     return m.group(1)
