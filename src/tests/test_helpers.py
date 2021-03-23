4e5a723d72d38839e4e77eeb01fd85a86e897da5
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2021-03-23 23:12:30 +0000

    Modify argument name from exception_name to exception_text as it was not the exception name but the entire text representation. Add test for the helper method.

--- src/tests/test_helpers.py
+++ src/tests/test_helpers.py
@@ -2,4 +2,6 @@ from src.helpers import get_exception_class
 
 
 def test_get_exception_class():
-    assert get_exception_class(exception_text='ValueError("some error")') == 'ValueError'
+    assert (
+        get_exception_class(exception_text='ValueError("some error")') == "ValueError"
+    )
