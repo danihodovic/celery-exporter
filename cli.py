2243862e324b966a308a327f4669e75f3b36b121
Adin Hodovic <hodovicadin@gmail.com>
2022-06-29 11:49:39 +0200

    fix: Change env var prefix and change service monitor port

--- cli.py
+++ cli.py
@@ -2,4 +2,4 @@ from src.cli import cli
 
 if __name__ == "__main__":
     # pylint: disable=no-value-for-parameter,unexpected-keyword-arg
-    cli(auto_envvar_prefix="CELERY_EXPORTER")
+    cli(auto_envvar_prefix="CE")
